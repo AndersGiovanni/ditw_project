5bf7bb5225469f6154da9dea4e99b1a6d13e7a12
hrmussa <andersgiovanni@gmail.com>
2021-11-29 15:08:34 +0100

    clustering, annotation, classification

---
+++ src/annotation_agreement.py
@@ -0,0 +1,102 @@
+from typing import Dict, List, Union
+from statsmodels.stats.inter_rater import fleiss_kappa
+from src.config import DATA_DIR
+from src.json_utils import read_jsonl
+from collections import Counter
+import numpy as np
+
+
+def encode_labels(labels: List[str]) -> List[str]:
+
+    labels_mapping = {
+        'NEGATIV': 0,
+        'NO SENTIMENT': 1,
+        'POSITIV': 2
+    }
+
+    new_labels = []
+    for label in labels:
+        new_labels.append(labels_mapping[label])
+
+    return new_labels
+
+
+def count_annotations(annotations: Dict[str, List[str]]) -> None:
+
+    names = list(annotations.keys())
+
+    for name in names:
+        print(f'{name}: {Counter(annotations[name])}')
+
+
+def compute_fleiss_kappa(annotations: Dict[str, List[str]]) -> None:
+
+    names = list(annotations.keys())
+
+    list_of_annotations = []
+
+    for a1, a2, a3, a4 in zip(annotations[names[0]], annotations[names[1]], annotations[names[2]], annotations[names[3]]):
+        list_of_annotations.append(encode_labels([a1, a2, a3, a4]))
+
+    # The input of fleiss is the count of each label pr row i.e. [3, 1, 0]
+    # means 3 negative, 1 no sentiment and 0 postive
+    fleiss_kappa_list_format = [[0, 0, 0] for i in range(len(list_of_annotations))]
+
+    for row, tweet_annotations in enumerate(list_of_annotations):
+        for column, count in list(Counter(tweet_annotations).items()):
+            fleiss_kappa_list_format[row][column] = count
+
+    print(fleiss_kappa(fleiss_kappa_list_format))
+
+
+def split_annotated_data(annotated_data: List[Dict]) -> Union[List[Dict], List[Dict]]:
+
+    ids_for_fleiss_kappa = [i[0] for i in list(Counter([i['id'] for i in annotated_data]).most_common(50)) if i[1] == 4]
+
+    evaluate_annotation_data = list()
+
+    sentiment_classification_data = list()
+
+    for annotation in annotated_data:
+        id_ = annotation['id']
+
+        if id_ in ids_for_fleiss_kappa:
+            evaluate_annotation_data.append(annotation)
+        else:
+            sentiment_classification_data.append(annotation)
+
+    return evaluate_annotation_data, sentiment_classification_data
+
+
+def preprocess_annotations(annotations: List[Dict]) -> Dict[str, List[str]]:
+
+    collect_annotations = {
+        'Anders': [],
+        'Ida': [],
+        'Johan': [],
+        'Nadia': []
+    }
+
+    for annotation in annotations:
+        label = annotation['label'][0]
+        annotator = annotation['annotation']
+        collect_annotations[annotator].append(label)
+
+    return collect_annotations
+
+
+if __name__ == '__main__':
+
+    annotation_file = DATA_DIR / 'all.jsonl'
+    annotations = read_jsonl(annotation_file)
+
+    evaluate_annotation_data, sentiment_classification_data = split_annotated_data(annotations)
+
+    collect_annotations = preprocess_annotations(evaluate_annotation_data)
+
+    count_annotations(collect_annotations)
+
+    compute_fleiss_kappa(collect_annotations)
+
+    a = 1
+    pass
---
+++ src/classification_model.py
@@ -0,0 +1,86 @@
+from typing import Dict, List, Union
+from numpy.lib.function_base import average
+from scipy.sparse.construct import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import TweetTokenizer
+import numpy as np
+from src.json_utils import read_jsonl
+from src.config import DATA_DIR
+from src.annotation_agreement import encode_labels, split_annotated_data
+from src.annotation_agreement import encode_labels, split_annotated_data
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+from sklearn.metrics import f1_score, classification_report
+from tqdm import tqdm
+
+
+def read_danish_stopwords() -> List[str]:
+    with open('data/danish_stopwords.txt') as stop_file:
+        return [word.strip() for word in stop_file.readlines()]
+
+
+def tf_idf_vectotize(text: List[str], stopwords: List[str]) -> np.ndarray:
+    tweet_tokenizer = TweetTokenizer().tokenize
+    vectorizer = TfidfVectorizer(
+        tokenizer=tweet_tokenizer,
+        stop_words=stopwords,
+        ngram_range=(
+            1,
+            5),
+        min_df=5,
+        max_df=0.80)
+    return vectorizer.fit_transform(text)
+
+
+def get_text_and_labels(annotations: List[Dict]) -> Union[List[str], List[str]]:
+    texts = list()
+    labels = list()
+
+    for annotation in annotations:
+        texts.append(annotation['data'])
+        labels.append(annotation['label'][0])
+
+    return texts, np.array(encode_labels(labels))
+
+
+if __name__ == '__main__':
+
+    annotation_file = DATA_DIR / 'all.jsonl'
+    annotations = read_jsonl(annotation_file)
+
+    stopwords = read_danish_stopwords()
+
+    evaluate_annotation_data, sentiment_classification_data = split_annotated_data(annotations)
+
+    texts, y = get_text_and_labels(sentiment_classification_data)
+
+    X = tf_idf_vectotize(texts, stopwords)
+
+    #kf = KFold(n_splits=5)
+    kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+    all_preds, all_trues = list(), list()
+    idx = 1
+    for train_index, test_index in kf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        #clf = RandomForestClassifier(max_depth=2, random_state=42, n_jobs=-1)
+        clf = LogisticRegression(random_state=42)
+
+        clf.fit(X_train, y_train)
+
+        preds = clf.predict(X_test)
+
+        f1 = f1_score(y_test, preds, average='weighted')
+
+        print(f'Fold: {idx}, F1: {f1}')
+
+        all_preds += preds.tolist()
+        all_trues += y_test.tolist()
+
+        idx += 1
+
+    target_names = ['Negative', 'No Sentiment', 'Positive']
+    print()
+    print(classification_report(all_trues, all_preds, target_names=target_names))
