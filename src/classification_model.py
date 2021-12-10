fc0368d25b2c6c73b0a411e4fa41fbd10c55f0c9
hrmussa <andersgiovanni@gmail.com>
2021-12-10 14:51:09 +0100

    twitter and folketinget cluster text. Confusion matrix

--- src/classification_model.py
+++ src/classification_model.py
@@ -9,10 +9,13 @@ from src.config import DATA_DIR
 from src.annotation_agreement import encode_labels, split_annotated_data
 from src.annotation_agreement import encode_labels, split_annotated_data
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 from sklearn.metrics import f1_score, classification_report
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+from danlp.models import load_bert_tone_model
 
 
 def read_danish_stopwords() -> List[str]:
@@ -20,7 +23,7 @@ def read_danish_stopwords() -> List[str]:
         return [word.strip() for word in stop_file.readlines()]
 
 
-def tf_idf_vectotize(text: List[str], stopwords: List[str]) -> np.ndarray:
+def tf_idf_vectotize(train: List[str], test: List[str], stopwords: List[str]) -> Union[np.ndarray, np.ndarray]:
     tweet_tokenizer = TweetTokenizer().tokenize
     vectorizer = TfidfVectorizer(
         tokenizer=tweet_tokenizer,
@@ -30,7 +33,9 @@ def tf_idf_vectotize(text: List[str], stopwords: List[str]) -> np.ndarray:
             3),
         min_df=3,
         max_df=0.80)
-    return vectorizer.fit_transform(text)
+    train_vec = vectorizer.fit_transform(train)
+    test_vec = vectorizer.transform(test)
+    return train_vec, test_vec
 
 
 def get_text_and_labels(annotations: List[Dict]) -> Union[List[str], List[str]]:
@@ -44,9 +49,24 @@ def get_text_and_labels(annotations: List[Dict]) -> Union[List[str], List[str]]:
     return texts, np.array(encode_labels(labels))
 
 
+def evaluate_BERT_tone(texts: List[str], labels: List[str]) -> None:
+
+    classifier = load_bert_tone_model()
+
+    target_names = ['Positive', 'No Sentiment', 'Negative']
+
+    predictions = []
+
+    for text in tqdm(texts):
+        largest_proba_id = np.argmax(classifier.predict_proba(text)[0])
+        predictions.append(largest_proba_id)
+
+    print(classification_report(labels, predictions, target_names=target_names))
+
+
 if __name__ == '__main__':
 
-    annotation_file = DATA_DIR / 'annotations/post/annotated_data_v3.jsonl'
+    annotation_file = DATA_DIR / 'annotations/post/annotated_data_v1.jsonl'
     annotations = read_jsonl(annotation_file)
 
     stopwords = read_danish_stopwords()
@@ -55,14 +75,14 @@ if __name__ == '__main__':
 
     texts, y = get_text_and_labels(sentiment_classification_data)
 
-    X = tf_idf_vectotize(texts, stopwords)
-
     #kf = KFold(n_splits=5)
-    kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     all_preds, all_trues = list(), list()
     idx = 1
-    for train_index, test_index in kf.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
+    for train_index, test_index in kf.split(texts, y):
+
+        X_train, X_test = np.array(texts)[train_index], np.array(texts)[test_index]
+        X_train, X_test = tf_idf_vectotize(X_train, X_test, stopwords)
         y_train, y_test = y[train_index], y[test_index]
 
         #clf = RandomForestClassifier(max_depth=2, random_state=42, n_jobs=-1)
@@ -81,6 +101,18 @@ if __name__ == '__main__':
 
         idx += 1
 
-    target_names = ['Negative', 'No Sentiment', 'Positive']
+    target_names = ['Negative', 'Neutral', 'Positive']
     print()
     print(classification_report(all_trues, all_preds, target_names=target_names))
+
+    cm = confusion_matrix(all_trues, all_preds, labels=clf.classes_, normalize='true')
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                  display_labels=target_names)
+    disp.plot(cmap=plt.get_cmap('Blues'))
+    # plt.tight_layout()
+    #plt.savefig('data/img/cm_v1.png', dpi=300)
+    plt.show()
+
+    print()
+    #print('BERT Tone Sentiment Classification')
+    #evaluate_BERT_tone(texts, y)
